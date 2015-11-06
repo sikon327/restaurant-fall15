b51fbec40eaeb07e3ba9444b8bbe57fac4345ab2
SI Loaner <siloaner@Lindys-MacBook-Air.local>
2015-11-06 16:00:46 -0500

    sign-in capability, owners

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,4 +1,5 @@
 class RestaurantsController < ApplicationController
+       before_action :authenticate_owner!, except:[:index, :show]
 	def index
 		@restaurants = Restaurant.all
 	end
---
+++ test/models/onwer_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class OnwerTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
